ddf125bc6a8bc3b255a32d613a0f0bb14b35b926
draftone <draftone@rustvillage.com>
2019-02-11 18:45:58 +0900

    add perceptron code

---
+++ perceptron.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Perceptron(object):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit_test(self, X, y):
+        rgen = np.random.seed(self.random_state)
+        print(rgen)
+
+    def fit(self, X, y):
+        #rgen = np.random.RandomState(self.random_state)
+        rgen = np.random.seed(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+def read_iris_data():
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+    print(df.tail())
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+    plt.scatter(X[:50,0], X[:50,1], color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker='x', label='versicolor')
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+if __name__ == "__main__":
+    read_iris_data()
+    per = Perceptron()
+    per.fit_test([2, 3], [2, 2])
+
